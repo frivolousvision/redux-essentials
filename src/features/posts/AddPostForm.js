fc2c33c7558964a9c1bb374734c0fc89d181adcb
Dan Durnell <dandurnell@Dan-Durnells-MBP.local>
2022-06-28 15:49:04 -0400

    completed redux async logic and data fethcing

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+//import { nanoid } from '@reduxjs/toolkit'
+
+import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
+
+export const AddPostForm = () => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
+  const [addRequestStatus, setAddRequestStatus] = useState('idle')
+
+  const dispatch = useDispatch()
+
+  const users = useSelector((state) => state.users)
+
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
+  const onAuthorChanged = (e) => setUserId(e.target.value)
+
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+
+  const onSavePostClicked = async () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus('pending')
+        await dispatch(addNewPost({ title, content, user: userId })).unwrap()
+        setTitle('')
+        setContent('')
+        setUserId('')
+      } catch (err) {
+        console.error('Failed to save post', err)
+      } finally {
+        setAddRequestStatus('idle')
+      }
+    }
+  }
+
+  const userOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
+
+  return (
+    <section>
+      <h2>Add a new Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          onChange={onTitleChanged}
+        ></input>
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {userOptions}
+        </select>
+        <label htmlFor="postCotent">Content:</label>
+        <input
+          type="text"
+          id="postContent"
+          name="postContent"
+          onChange={onContentChanged}
+        ></input>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Posts
+        </button>
+      </form>
+    </section>
+  )
+}
